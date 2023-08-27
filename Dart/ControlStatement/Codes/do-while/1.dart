110eed7fbef80212cd834870fcf1f4f1ed334d1a
Tushar Kokane <tusharkokane982@gmail.com>
2023-08-27 23:54:43 +0530

    switch -case and do-while

---
+++ Dart/ControlStatement/Codes/do-while/1.dart
@@ -0,0 +1,7 @@
+void main(){
+  int x=10;
+  do{
+    print(x);
+  }while(x>10);
+
+}
---
+++ Dart/ControlStatement/Codes/do-while/2.dart
@@ -0,0 +1,24 @@
+void main(){
+  int x=1;
+  do{
+    print("1:C");
+     print("2:Cpp");
+      print("3:Java");
+       print("4:dart");
+
+       switch(x){
+        case 1:
+        print("1972");
+        case 2:
+        print("1975");
+        case 3:
+        print("1990");
+        case 4:
+        print("2013");
+        default:
+        print("nothing to print");
+       }
+       
+
+  }while(false);
+}
